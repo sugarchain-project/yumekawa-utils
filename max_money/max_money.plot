1f4181386032f47e8a5c9f11c58b7a90ed00014c
cryptozeny <37016180+cryptozeny@users.noreply.github.com>
2019-03-26 03:46:03 +0900

    FIX: Halving: init reward "2^32" and interval exact 2 years (#1)
    Block Time:		5 Seconds
    
    Initial Block Reward:	`4294967296 Satoshis`
    Initial Block Reward:	42.94967296 COINs
    
    Halving Interval:	`12614400 Blocks`
    * in Years:		  `2 Years`
    * in Days:		  730 Days
    * in Hours:		  17520 Hours
    * in Minutes:		  1051200 Minutes
    * in Seconds:		  63072000 Seconds
    
    Total Halving Count:	33 Times
    * in Years:		  66 Years
    * in Days:		  24090 Days
    * in Hours:		  578160 Hours
    * in Minutes:		  34689600 Minutes
    * in Seconds:		  2081376000 Seconds
    
    Total Satoshis:	`108356870904710400 Satoshis`
    Total COINs:		1083568709.047104 COINs
    
    ```
    Count	Supply			Reward
    0	0			4294967296
    1	54178435458662400	2147483648
    2	81267653187993600	1073741824
    3	94812262052659200	536870912
    4	101584566484992000	268435456
    5	104970718701158400	134217728
    6	106663794809241600	67108864
    7	107510332863283200	33554432
    8	107933601890304000	16777216
    9	108145236403814400	8388608
    10	108251053660569600	4194304
    11	108303962288947200	2097152
    12	108330416603136000	1048576
    13	108343643760230400	524288
    14	108350257338777600	262144
    15	108353564128051200	131072
    16	108355217522688000	65536
    17	108356044220006400	32768
    18	108356457568665600	16384
    19	108356664242995200	8192
    20	108356767580160000	4096
    21	108356819248742400	2048
    22	108356845083033600	1024
    23	108356858000179200	512
    24	108356864458752000	256
    25	108356867688038400	128
    26	108356869302681600	64
    27	108356870110003200	32
    28	108356870513664000	16
    29	108356870715494400	8
    30	108356870816409600	4
    31	108356870866867200	2
    32	108356870892096000	1
    33	108356870904710400	0
    ```

--- max_money/max_money.plot
+++ max_money/max_money.plot
@@ -1,34 +1,33 @@
 #!/usr/bin/gnuplot 
 
-# QT
-set term qt size 1200, 600;
+# do QT size
+set terminal qt size 1200,600;
 
-# PNG
+# do PNG
 # set term png size 1200, 600;
 # set output 'max_money.png';
 
-set title "Max Money SUGAR\n{/*0.75 Halving Interval = 210000*120 = 25200000 (4y)}" font ",15";
+set title "Max Money SUGAR\n \
+    {/*0.75\
+    Block Time = 5 Seconds,\
+    Initial Block Reward = 2^{32} = 4294967296 Satoshis,\
+    Halving Interval = 210240/2*120 = 12614400 (2 Years)\
+    }" font ",15";
 set key center right box;
 
-HALVING_COUNT="9";
-set xrange [0:HALVING_COUNT];
+set xrange [0:*];
 set xtics 0, 1;
 set xlabel "Halving Count" tc rgb "black" offset 0;
 
-MAX_MONEY="2520000000e+8"; # 252000000000000000
 set lmargin at screen 0.15;
-set yrange [0:MAX_MONEY];
-set ytics 0, 1260000000e+8; # 126000000000000000
-set ytics add (1890000000e+8) (2205000000e+8) (2362500000e+8) (2441250000e+8);
+set yrange [0:*];
 set format y "%.0f";
 set ylabel "Total Supply (in Satoshis)" tc rgb "red" offset -3;
 set ytics nomirror;
 
-BLOCK_REWARD="5e+9"; # 5000000000
 set rmargin at screen 0.89;
-set y2range [0:BLOCK_REWARD];
-set y2tics 0, BLOCK_REWARD/2; 
-set y2tics add (BLOCK_REWARD/4) (BLOCK_REWARD/8) (BLOCK_REWARD/16) (BLOCK_REWARD/32);
+set y2range [0:*];
+set y2tics 0, 1e+9; 
 set format y2 "%.0f";
 set y2label "Block Reward (in Satoshis)" tc rgb "blue" offset 2.5;
 
@@ -44,8 +43,6 @@ set style line 2 \
     linetype 1 linewidth 2 \
     pointtype 2 pointsize 1.5
 
-# set offset 1,1,1,1;
-
 plot \
     'max_money.csv' using 1:2 axis x1y1 with linespoints linestyle 1 title "Total Supply", \
     ''              using 1:3 axis x1y2 with steps linestyle 2 title "Block Reward", \
@@ -53,4 +50,22 @@ plot \
     ''              using 1:2:(sprintf("%0.f", $2)) with labels rotate by 315 left offset 1.2,-0.5 tc rgb '#990000' notitle, \
     ''              using 1:3:(sprintf("%0.f", $3)) axis x1y2 with labels rotate by 0 left offset 2,0.75 tc rgb '#000099' notitle, \
 
-pause -1
+# do setting min max range
+set yrange [GPVAL_DATA_Y_MIN:GPVAL_DATA_Y_MAX];
+set ytics 0, GPVAL_DATA_Y_MAX/2; # half of max_money
+set y2range [GPVAL_DATA_Y2_MIN:GPVAL_DATA_Y2_MAX];
+set y2tics 0, GPVAL_DATA_Y2_MAX/2; # half of init reward
+
+# do resetting x-axis
+set xrange [0:9];
+
+# do tics
+set tics scale 2.5, 1;
+set mxtics 2;
+set mytics 2;
+set my2tics 2;
+
+# do re-plot'ing
+replot;
+    
+pause -1;
--- max_money/max_money.py
+++ max_money/max_money.py
@@ -11,11 +11,34 @@ import sys # https://stackoverflow.com/questions/34926517/stop-sys-stdout-from-w
 orig_stdout = sys.stdout
 sys.stdout=open("max_money.csv", "w")
 
+# note - BTC original halving is `3.9954337899543378996`
+"""
+>>> interval=np.float128(210000.0)
+>>> interval*600/3600/24/365
+3.9954337899543378996
+"""
+
+# note - There are a maximum of 2,099,999,997,690,000 Bitcoin in Satoshis
+# Total BTC is 2099999997690000 Satoshis
+# https://en.bitcoin.it/wiki/Bitcoin
+
 # setup
-current_reward = np.int64(50 * 10**8) # 50 SUGAR = 50 0000 0000 Satoshis
-reward_interval = np.int64(210000 * 120) # 210000*120 = 25200000 is around every 4 years with a 5 seconds block interval
-total = np.int64(0)
 halving_count = np.int64(0)
+total = np.int64(0)
+
+# current_reward = np.float128(50 * 10**8) # BTC float128 # NOT CORRECT!!
+# current_reward = np.int64(50 * 10**8) # BTC int64
+current_reward = np.float128(4294967296) # 2^32 = 42 9496 7296 Satoshis
+init_reward_printer = current_reward # store it for print after
+
+# reward_interval = np.int64(210000) # BTC about 4 years
+# reward_interval = np.int64(210240) # BTC exactly 4 years: 3600*24/600*365*4 = 210240
+# reward_interval = np.int64(210000 * 120) # 210000*120 = 25200000 is around about 4 years with a 5 seconds block interval
+# reward_interval = np.int64(210240 * 120) # 210240*120 = 25228800 is around exactly 4 years with a 5 seconds block interval
+reward_interval = np.int64(210240 / 2 * 120) # 210240/2*120 = 12614400 is around exactly 2 years with a 5 seconds block interval
+
+# blocktime = np.int64(600) # BTC 10 minutes
+blocktime = np.int64(5) # 5 seconds for sugarchain
 
 # print header
 print "Count\tSupply\t\t\tReward"
@@ -24,61 +47,106 @@ print "%d\t\t\t" % total, # current supply is 0
 print "%d" % current_reward
 
 # main loop
-while current_reward > 0: # check current if reward is not zero
+# while current_reward > 0 and halving_count < 64: # BTC
+while current_reward > 0 and halving_count < 35: # TEST SUGAR
     halving_count += 1
     print "%d\t" % halving_count,
     total += reward_interval * current_reward
     print "%d\t" % total, # current supply is going bigger to max_money
-    current_reward /= 2
-    print "%d" % current_reward
+
+    # correction for float128: it makes 1 to 0 satoshi.
+    if current_reward <= 1.0:
+        current_reward = 0
+    else:
+        current_reward /= 2
+
+    print "%.32g" % current_reward
 
 # close - print to file
 sys.stdout.close()
 sys.stdout=orig_stdout
 
-# print total
-print "Total SUGAR to ever be created: %d" % total, "Satoshis"
-print "Total SUGAR to ever be created: %.3f" % (total/1e+8), "SUGAR (rounded)"
+# print result
+print ""
+# blocktime
+print "  Block Time:\t\t%d" % blocktime, "Seconds"
+# reward
+print "  Initial Block Reward:\t%d" % (np.float128(init_reward_printer)), "Satoshis"
+print "  Initial Block Reward:\t%.8f" % (np.float128(init_reward_printer)/1e+8), "COINs"
+# reward_interval
+print "  Halving Interval:\t%d" % reward_interval, "Blocks"
+print "  * in Years:\t\t  %.16g" % (np.float128(reward_interval) * blocktime / 3600 / 24 / 365), "Years"
+print "  * in Days:\t\t  %.16g" % (np.float128(reward_interval) * blocktime / 3600 / 24), "Days"
+print "  * in Hours:\t\t  %.16g" % (np.float128(reward_interval) * blocktime / 3600), "Hours"
+print "  * in Minutes:\t\t  %.16g" % (np.float128(reward_interval) * blocktime / 60), "Minutes"
+print "  * in Seconds:\t\t  %.16g" % (np.float128(reward_interval) * blocktime), "Seconds"
+# halving_count
+print "  Total Halving Count:\t%d" % halving_count, "Times"
+print "  * in Years:\t\t  %.16g" % ( halving_count * (np.float128(reward_interval) * blocktime / 3600 / 24 / 365) ), "Years"
+print "  * in Days:\t\t  %.16g" % ( halving_count * (np.float128(reward_interval) * blocktime / 3600 / 24) ), "Days"
+print "  * in Hours:\t\t  %.16g" % ( halving_count * (np.float128(reward_interval) * blocktime / 3600) ), "Hours"
+print "  * in Minutes:\t\t  %.16g" % ( halving_count * (np.float128(reward_interval) * blocktime / 60) ), "Minutes"
+print "  * in Seconds:\t\t  %.16g" % ( halving_count * (np.float128(reward_interval) * blocktime) ), "Seconds"
+# total
+print "  Total Satoshis:\t%d" % total, "Satoshis"
+print "  Total COINs:\t\t%f" % (total/1e+8), "COINs" # %.6f but it works. because %.8f NOT WOKRED!
+print ""
 
-
-# output example - SUGAR
+# output example
 """
-$ ./max_money.py && cat max_money.csv
-Total SUGAR to ever be created: 251999999722800000 Satoshis
-Total SUGAR to ever be created: 2519999997 SUGAR (rounded)
+yumekawa-utils$ cd max_money && ./max_money.py && cat ./max_money.csv && gnuplot ./max_money.plot; cd ..
+
+  Block Time:		5 Seconds
+  Initial Block Reward:	4294967296 Satoshis
+  Initial Block Reward:	42.94967296 COINs
+  Halving Interval:	12614400 Blocks
+  * in Years:		  2 Years
+  * in Days:		  730 Days
+  * in Hours:		  17520 Hours
+  * in Minutes:		  1051200 Minutes
+  * in Seconds:		  63072000 Seconds
+  Total Halving Count:	33 Times
+  * in Years:		  66 Years
+  * in Days:		  24090 Days
+  * in Hours:		  578160 Hours
+  * in Minutes:		  34689600 Minutes
+  * in Seconds:		  2081376000 Seconds
+  Total Satoshis:	108356870904710400 Satoshis
+  Total COINs:		1083568709.047104 COINs
+
 Count	Supply			Reward
-0	0			5000000000
-1	126000000000000000	2500000000
-2	189000000000000000	1250000000
-3	220500000000000000	625000000
-4	236250000000000000	312500000
-5	244125000000000000	156250000
-6	248062500000000000	78125000
-7	250031250000000000	39062500
-8	251015625000000000	19531250
-9	251507812500000000	9765625
-10	251753906250000000	4882812
-11	251876953112400000	2441406
-12	251938476543600000	1220703
-13	251969238259200000	610351
-14	251984619104400000	305175
-15	251992309514400000	152587
-16	251996154706800000	76293
-17	251998077290400000	38146
-18	251999038569600000	19073
-19	251999519209200000	9536
-20	251999759516400000	4768
-21	251999879670000000	2384
-22	251999939746800000	1192
-23	251999969785200000	596
-24	251999984804400000	298
-25	251999992314000000	149
-26	251999996068800000	74
-27	251999997933600000	37
-28	251999998866000000	18
-29	251999999319600000	9
-30	251999999546400000	4
-31	251999999647200000	2
-32	251999999697600000	1
-33	251999999722800000	0
+0	0			4294967296
+1	54178435458662400	2147483648
+2	81267653187993600	1073741824
+3	94812262052659200	536870912
+4	101584566484992000	268435456
+5	104970718701158400	134217728
+6	106663794809241600	67108864
+7	107510332863283200	33554432
+8	107933601890304000	16777216
+9	108145236403814400	8388608
+10	108251053660569600	4194304
+11	108303962288947200	2097152
+12	108330416603136000	1048576
+13	108343643760230400	524288
+14	108350257338777600	262144
+15	108353564128051200	131072
+16	108355217522688000	65536
+17	108356044220006400	32768
+18	108356457568665600	16384
+19	108356664242995200	8192
+20	108356767580160000	4096
+21	108356819248742400	2048
+22	108356845083033600	1024
+23	108356858000179200	512
+24	108356864458752000	256
+25	108356867688038400	128
+26	108356869302681600	64
+27	108356870110003200	32
+28	108356870513664000	16
+29	108356870715494400	8
+30	108356870816409600	4
+31	108356870866867200	2
+32	108356870892096000	1
+33	108356870904710400	0
 """
